02e17698ba049c97c9b11fbd4f590c5c454b8808
Argyaboy20 <smpn21985@gmail.com>
2025-02-27 16:30:43 +0700

    mencoba routing halaman yang lebih baik di mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {
@@ -35,7 +35,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
+      preloadingStrategy: PreloadAllModules, useHash: true,
       scrollPositionRestoration: 'enabled'
     })
   ],
