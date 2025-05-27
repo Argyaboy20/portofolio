f17b3328db399c0c55c8d8a4eb5e846ff087b489
Argyaboy20 <smpn21985@gmail.com>
2025-05-27 11:55:41 +0700

    - memperbarui halaman aboutme - memperbarui beberapa halaman seperti admin, biodata, locana, relawan, tab1, tab2, tab3, dan tab4

--- src/app/aboutme/aboutme.page.ts
+++ src/app/aboutme/aboutme.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-aboutme',
@@ -8,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutmePage implements OnInit {
 
-  constructor() { }
+  /* Language toggle properties */
+  isEnglish = false; // Default is Indonesian
+  
+  /* Scroll button visibility */
+  showScrollButton = false;
+  
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    /* Check scroll position on init */
+    this.checkScrollPosition();
   }
-
+  
+  /**
+   * Toggle between English and Indonesian language
+   */
+  toggleLanguage() {
+    this.isEnglish = !this.isEnglish;
+  }
+  
+  /**
+   * Scroll to top function for the arrow button
+   */
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+  /**
+   * Listen for scroll events to show/hide the scroll button
+   */
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.checkScrollPosition();
+  }
+  
+  /**
+   * Check scroll position and update button visibility
+   */
+  private checkScrollPosition() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    
+    /* Show button only when scrolled down at least 300px */
+    this.showScrollButton = scrollPosition > 300;
+  }
 }
